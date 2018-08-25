4c56172566f69cdeab35b7f1310813bbd855f0a8
Chris Kozlowski <kozlowskicd@gmail.com>
2018-08-25 14:48:44 -0700

    Build /books page and worked on CSS styling

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ app.set('view engine', 'ejs');
 const PORT = process.env.PORT;
 
 // const conString = 'postgres://qprfdlcbagobrk:8e859511a13c3a26fa2ce9f58d793f7d86f711e0ed6f39704546bcdb678eaf86@ec2-50-17-194-129.compute-1.amazonaws.com:5432/dej042teb9sea6';
-const client = new pg.Client(process.env.DATABASE_URL)
-    // {user: "qprfdlcbagobrk",
-    // password: "8e859511a13c3a26fa2ce9f58d793f7d86f711e0ed6f39704546bcdb678eaf86",
-    // database: "dej042teb9sea6",
-    // port: 5432,
-    // host: "ec2-50-17-194-129.compute-1.amazonaws.com",
-    // ssl: true
-// }); 
+const client = new pg.Client({//process.env.DATABASE_URL)
+    user: "qprfdlcbagobrk",
+    password: "8e859511a13c3a26fa2ce9f58d793f7d86f711e0ed6f39704546bcdb678eaf86",
+    database: "dej042teb9sea6",
+    port: 5432,
+    host: "ec2-50-17-194-129.compute-1.amazonaws.com",
+    ssl: true
+}); 
 client.connect();
 client.on('error', error => {
   console.error(error);
